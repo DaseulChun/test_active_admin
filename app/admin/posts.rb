1ee234905bfbe38836f532745b466092583c8149
DaseulChun <nandaseul@gmail.com>
2020-09-08 14:56:24 -0700

    Add User and Post

---
+++ app/admin/posts.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Post do
+  permit_params :title, :body, :published_at, :user_id
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_204557) do
+ActiveRecord::Schema.define(version: 2020_09_08_214440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,22 @@ ActiveRecord::Schema.define(version: 2020_09_08_204557) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "published_at"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
